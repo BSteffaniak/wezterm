b006f066eb5358862d5ff2fbafddfa4d4dfe9b95
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-09-13 07:30:00 -0400

    Add window_padding 0

--- wezterm.lua
+++ wezterm.lua
@@ -18,5 +18,12 @@ config.hide_tab_bar_if_only_one_tab = true
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 -- and finally, return the configuration to wezterm
 return config
